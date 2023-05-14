4a53f67c0575428f023d859ede70c9e74f753bd5
Alex Engel <arengel@users.noreply.github.com>
2023-05-14 14:18:35 -0600

    after rebase

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -21,12 +21,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    return vec;
 }
